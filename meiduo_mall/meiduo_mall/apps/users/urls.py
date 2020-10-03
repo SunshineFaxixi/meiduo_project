6c6de28819b83d2354f82e3d8bbf6a0b7204019e
Sunshine <xxhan2018@163.com>
2020-10-03 17:11:42 +0800

    收货地址-新增地址-保存地址数据

--- meiduo_mall/meiduo_mall/apps/users/urls.py
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     url(r'^emails/$', views.EmailView.as_view()),
     url(r'^emails/verification/', views.VerifyEmailView.as_view()),
     url(r'^addresses/$', views.AddressView.as_view(), name='address'),
+    url(r'^addresses/create/$', views.AddressCreateView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/users/views.py
+++ meiduo_mall/meiduo_mall/apps/users/views.py
@@ -17,7 +17,14 @@ from .utils import generate_verify_email_url, check_verify_email_token
 
 logger = logging.getLogger('django')
 
-class AddressView(View):
+
+class AddressCreateView(LoginRequiredJSONMixin, View):
+    """新增地址"""
+    def post(self, request):
+        """实现新增地址逻辑"""
+        pass
+
+class AddressView(LoginRequiredMixin, View):
     """收货地址"""
     def get(self, request):
         return render(request, 'user_center_site.html')
