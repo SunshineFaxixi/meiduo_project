955e2fedc8bda775f7af91746a263b843c1c07ed
Sunshine <xxhan2018@163.com>
2020-10-07 20:34:55 +0800

    商品列表页-封装获取商品分类的方法

--- meiduo_mall/meiduo_mall/apps/contents/views.py
+++ meiduo_mall/meiduo_mall/apps/contents/views.py
@@ -5,36 +5,13 @@ from django.views import View
 
 from goods.models import GoodsChannel
 from .models import ContentCategory
+from .utils import get_categories
+
 
 # Create your views here.
 class IndexView(View):
     def get(self, request):
-        # 准备商品分类对应的字典
-        categories = OrderedDict()
-        # 查询出所有的商品频道: 37个一级类别
-        channels = GoodsChannel.objects.order_by('group_id', 'sequence')
-        # 遍历所有频道
-        for channel in channels:
-            # 获取当前频道所在的组
-            group_id = channel.group_id
-            # 构造基本的数据框架：只有11个组
-            if group_id not in categories:
-                categories[group_id] = {"channels": [], "sub_cats": []}
-
-            cat1 = channel.category
-            categories[group_id]["channels"].append({
-                "id": channel.id,
-                "name": cat1.name,
-                "url": channel.url
-            })
-
-            for cat2 in cat1.subs.all():
-                cat2.sub_cats = []
-                for cat3 in cat2.subs.all():
-                    cat2.sub_cats.append(cat3)
-
-                categories[group_id]["sub_cats"].append(cat2)
-
+        categories = get_categories()
         # 查询首页广告数据
         contents = {}
         # 查询所有的广告类别
--- meiduo_mall/meiduo_mall/apps/goods/views.py
+++ meiduo_mall/meiduo_mall/apps/goods/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 
+from contents.utils import get_categories
 
 # Create your views here.
 class ListView(View):
     """商品列表页"""
     def get(self, request, category_id, page_num):
-        return render(request, 'list.html')
+        categories = get_categories()
+        context = {
+            'categories': categories,
+        }
+        return render(request, 'list.html', context)
